da2b9f74f81b3eae6a4b9aaf66fd173e4bc4b009
MKStrandholm <markkawucha@gmail.com>
2025-09-23 22:13:03 -0400

    Router update

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,35 +2,36 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Index from '../views/Index.vue'
 import ErrorPage from "../views/ErrorPage.vue"
 
-    const routes = [
-      {
-        path: "/:catchAll(.*)",
-        name: "NotFound",
-        component: ErrorPage,
-        meta: {
-          requiresAuth: false
-        }
-      },
-      {
-        path: '/',
-        name: 'index',
-        component: Index
-      },
-      {
-        path: '/:id',
-        name: 'game',
-        component: Index
-      },
-      {
-        path: '/error',
-        name: 'error',
-        component: ErrorPage
-      },
-    ]
+const routes = [
+  {
+    path: '/',
+    name: 'index',
+    component: Index
+  },
+  {
+    path: '/:id',
+    name: 'game',
+    component: Index
+  },
+  {
+    path: '/error',
+    name: 'error',
+    component: ErrorPage
+  },
+  // Catch-all must come last
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: ErrorPage,
+    meta: {
+      requiresAuth: false
+    }
+  },
+]
 
-    const router = createRouter({
-      history: createWebHistory(process.env.BASE_URL),
-      routes
-    })
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
 
-    export default router;
+export default router;
